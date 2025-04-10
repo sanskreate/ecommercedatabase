6f2ac637f7a77a6eaa049f2aca43dd0e312f039f
sanskreate <123264981+sanskreate@users.noreply.github.com>
2025-04-11 01:21:37 +0530

    Create OnlineShopping.sql

---
+++ db and table creation/OnlineShopping.sql
@@ -0,0 +1,14 @@
+-- Database: OnlineShopping
+
+-- DROP DATABASE IF EXISTS "OnlineShopping";
+
+CREATE DATABASE "OnlineShopping"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en-US'
+    LC_CTYPE = 'en-US'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
