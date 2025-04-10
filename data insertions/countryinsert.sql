f889d483d1f6e558006bc378dad509ac7a4f49d7
sanskreate <123264981+sanskreate@users.noreply.github.com>
2025-04-11 01:34:47 +0530

    Create countryinsert.sql

---
+++ data insertions/countryinsert.sql
@@ -0,0 +1,13 @@
+INSERT INTO public.country(
+	countryid, countryname)
+	VALUES 
+(1, 'United States'),
+(2, 'Canada'),
+(3, 'United Kingdom'),
+(4, 'Australia'),
+(5, 'Germany'),
+(6, 'France'),
+(7, 'India'),
+(8, 'Brazil'),
+(9, 'Japan'),
+(10, 'South Korea');
