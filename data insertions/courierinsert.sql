b194c1f74211902eb2b02bca6d1097b1387c3b46
sanskreate <123264981+sanskreate@users.noreply.github.com>
2025-04-11 01:35:17 +0530

    Create courierinsert.sql

---
+++ data insertions/courierinsert.sql
@@ -0,0 +1,13 @@
+INSERT INTO public.courier(
+	courierid, name, contact)
+	VALUES
+(1, 'BlueDart Express', '1800-233-1234'),
+(2, 'Delhivery', '1800-103-4567'),
+(3, 'DTDC Courier', '1860-425-4255'),
+(4, 'FedEx India', '1800-209-6161'),
+(5, 'India Post', '1800-266-6868'),
+(6, 'Ecom Express', '1800-123-4560'),
+(7, 'Shadowfax', '1800-456-7890'),
+(8, 'XpressBees', '1800-200-2800'),
+(9, 'Gati Ltd.', '1860-123-4284'),
+(10, 'Amazon Transportation Services', '1800-300-9009');
